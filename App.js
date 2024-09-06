bb9825183b664787afb4370b80a1f7e2dc59c287
abhirawatt786 <42902553+abhirawatt786@users.noreply.github.com>
2024-09-06 21:48:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import ImageDetailsScreen from './screens/ImageDetailsScreen';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="ImageDetails" component={ImageDetailsScreen} options={{ title: 'Image Details' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
